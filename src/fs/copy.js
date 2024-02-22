e4653eee89bab06c7c559307ce8fcc2db8ff48c7
urichkaa <121604424+urichkaa@users.noreply.github.com>
2024-02-22 10:09:40 +0300

    Update copy.js
    я использовал fs.existsSync для проверки существования папок, fs.mkdirSync для создания целевой папки, fs.readdirSync для чтения содержимого исходной папки и fs.copyFileSync для копирования файлов

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,33 @@
 const copy = async () => {
-    // Write your code here 
+    const sourceFolder = 'files';
+  const destinationFolder = 'files_copy';
+
+  
+  if (!fs.existsSync(sourceFolder)) {
+    throw new Error('FS operation failed: Source folder does not exist');
+  }
+
+  
+  if (fs.existsSync(destinationFolder)) {
+    throw new Error('FS operation failed: Destination folder already exists');
+  }
+
+ 
+  fs.mkdirSync(destinationFolder);
+
+
+  const files = fs.readdirSync(sourceFolder);
+
+
+  files.forEach((file) => {
+    const sourcePath = path.join(sourceFolder, file);
+    const destinationPath = path.join(destinationFolder, file);
+
+   
+    fs.copyFileSync(sourcePath, destinationPath);
+  });
+
+  console.log('Files copied successfully!');
 };
 
 await copy();
