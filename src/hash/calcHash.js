96b6a3bde863339b0d8633b478526068f05c3b3a
urichkaa <121604424+urichkaa@users.noreply.github.com>
2024-02-22 10:22:50 +0300

    Update calcHash.js
    В этой реализации мы используем модуль crypto для вычисления хэша SHA256. Сначала мы считываем содержимое файла fileToCalculateHashFor.txt с помощью функции readFile из модуля fs/promises. Затем мы создаем экземпляр хэша, используя createHash('sha256'). Мы обновляем хэш считанным содержимым файла с помощью hash.update(fileContent). Затем мы вычисляем хэш в формате шестнадцатеричного числа (hex) с помощью hash.digest('hex').
    
    Наконец, мы выводим вычисленный хэш в консоль с помощью console.log. В случае ошибки при вычислении хэша, мы выводим сообщение об ошибке в консоль с помощью console.error.

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const filePath = 'fileToCalculateHashFor.txt';
+    const fileContent = await fs.readFile(filePath);
+
+    const hash = createHash('sha256');
+    hash.update(fileContent);
+    const hashHex = hash.digest('hex');
+
+    console.log('SHA256 Hash:', hashHex);
+  } catch (error) {
+    console.error('Error calculating hash:', error);
+  } 
 };
 
 await calculateHash();
+
+
+
