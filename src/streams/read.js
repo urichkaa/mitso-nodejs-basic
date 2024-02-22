ed951313a46a5b02f03b8150934850fc62f8ebcb
urichkaa <121604424+urichkaa@users.noreply.github.com>
2024-02-22 10:26:48 +0300

    Update read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
 const read = async () => {
-    // Write your code here 
+  try {
+    const filePath = 'fileToRead.txt';
+    const readStream = createReadStream(filePath, { encoding: 'utf-8' });
+
+    readStream.on('data', (chunk) => {
+      process.stdout.write(chunk);
+    });
+
+    readStream.on('end', () => {
+      console.log('File reading completed.');
+    });
+  } catch (error) {
+    console.error('Error reading file:', error);
+  }
 };
 
 await read();
