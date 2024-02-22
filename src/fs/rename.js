ac031d1a25e9a9244aadf60cff30658c34a26bca
urichkaa <121604424+urichkaa@users.noreply.github.com>
2024-02-22 10:10:47 +0300

    Update rename.js
    я использовал fs.existsSync для проверки существования исходного файла и целевого файла, и fs.renameSync для переименования файла. Если файл wrongFilename.txt не существует или файл properFilename.md уже существует, будет выброшена ошибка с соответствующим сообщением.

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
 const rename = async () => {
-    // Write your code here 
+  const sourceFile = 'wrongFilename.txt';
+  const destinationFile = 'properFilename.md';
+
+
+  if (!fs.existsSync(sourceFile)) {
+    throw new Error('FS operation failed: Source file does not exist');
+  }
+
+
+  if (fs.existsSync(destinationFile)) {
+    throw new Error('FS operation failed: Destination file already exists');
+  }
+
+
+  fs.renameSync(sourceFile, destinationFile);
+
+  console.log('File renamed successfully!');
 };
 
 await rename();
