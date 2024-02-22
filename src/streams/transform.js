1d8ad3fe7e8cdc2a3f0926a1a712ff611865d3a6
urichkaa <121604424+urichkaa@users.noreply.github.com>
2024-02-22 10:27:24 +0300

    Update transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
 const transform = async () => {
-    // Write your code here 
+  try {
+    const reverseTransform = new Transform({
+      transform(chunk, encoding, callback) {
+        const reversedChunk = chunk.toString().split('').reverse().join('');
+        this.push(reversedChunk);
+        callback();
+      },
+    });
+
+    process.stdin.pipe(reverseTransform).pipe(process.stdout);
+
+    console.log('Enter text to reverse (Press Ctrl+D to finish):');
+  } catch (error) {
+    console.error('Error transforming data:', error);
+  }
 };
 
 await transform();
