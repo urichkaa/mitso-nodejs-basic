79663d4aafc28bac51dd4221367c79a152afbeb6
urichkaa <121604424+urichkaa@users.noreply.github.com>
2024-02-22 10:15:21 +0300

    Update args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,21 @@
 const parseArgs = () => {
-    // Write your code here 
+
+  const args = process.argv.slice(2);
+
+  const parsedArgs = {};
+
+
+  for (let i = 0; i < args.length; i += 2) {
+    const propName = args[i].slice(2); // Удаляем префикс "--"
+    const propValue = args[i + 1];
+    parsedArgs[propName] = propValue;
+  }
+
+
+  Object.keys(parsedArgs).forEach((propName) => {
+    const propValue = parsedArgs[propName];
+    console.log(`${propName} is ${propValue}`);
+  });
 };
 
 parseArgs();
