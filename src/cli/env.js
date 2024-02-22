36677e2f680466ac0589750b529dc9e34dbbf662
urichkaa <121604424+urichkaa@users.noreply.github.com>
2024-02-22 10:14:13 +0300

    Update env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+  
+  const envVariables = process.env;
+ const mitsoVariables = Object.keys(envVariables).filter((key) =>
+    key.startsWith('MITSO_')
+  );
+ const formattedEnv = mitsoVariables
+    .map((key) => `MITSO_${key}=${envVariables[key]}`)
+    .join('; ');
+    console.log(formattedEnv);
 };
 
 parseEnv();
