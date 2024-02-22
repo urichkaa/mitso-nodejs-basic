6c8f65a0cce91aab51488fa219b4495fdba7616b
urichkaa <121604424+urichkaa@users.noreply.github.com>
2024-02-22 10:28:36 +0300

    Update decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+  try {
+    const sourceFilePath = 'archive.gz';
+    const destinationFilePath = 'fileToCompress.txt';
+
+    const readStream = createReadStream(sourceFilePath);
+    const gunzip = createGunzip();
+    const writeStream = createWriteStream(destinationFilePath);
+
+    readStream.pipe(gunzip).pipe(writeStream);
+
+    console.log('File decompression completed.');
+  } catch (error) {
+    console.error('Error decompressing file:', error);
+  }
 };
 
 await decompress();
