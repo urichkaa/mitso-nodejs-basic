32f73e5ebf5bb2fb2f8e117321d58fbbad126904
urichkaa <121604424+urichkaa@users.noreply.github.com>
2024-02-22 10:28:15 +0300

    Update compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+  try {
+    const sourceFilePath = 'fileToCompress.txt';
+    const destinationFilePath = 'archive.gz';
+
+    const readStream = createReadStream(sourceFilePath);
+    const gzip = createGzip();
+    const writeStream = createWriteStream(destinationFilePath);
+
+    readStream.pipe(gzip).pipe(writeStream);
+
+    console.log('File compression completed.');
+  } catch (error) {
+    console.error('Error compressing file:', error);
+  }
 };
 
 await compress();
