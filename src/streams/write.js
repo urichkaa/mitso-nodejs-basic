320ade2f403500e72b4075b4e7229d2f336b28e1
urichkaa <121604424+urichkaa@users.noreply.github.com>
2024-02-22 10:25:35 +0300

    Update write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
-const write = async () => {
-    // Write your code here 
+const read = async () => {
+  try {
+    const filePath = 'fileToRead.txt';
+    const readStream = createReadStream(filePath, { encoding: 'utf-8' });
+
+    readStream.on('data', (chunk) => {
+      process.stdout.write(chunk);
+    });
+
+    readStream.on('end', () => {
+      console.log('File reading completed.');
+    });
+  } catch (error) {
+    console.error('Error reading file:', error);
+  }
 };
 
-await write();
+await read();
