b7099dfcf2b4d89776c3bb76e81bdefcef28707c
urichkaa <121604424+urichkaa@users.noreply.github.com>
2024-02-22 10:12:53 +0300

    Update read.js
    я использовал fs.existsSync для проверки существования файла fileToRead.txt и fs.readFileSync для чтения его содержимого. Если файл не существует, будет выброшена ошибка с соответствующим сообщением. Содержимое файла выводится в консоль.

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
 const read = async () => {
-    // Write your code here 
+  const fileToRead = 'fileToRead.txt';
+
+
+  if (!fs.existsSync(fileToRead)) {
+    throw new Error('FS operation failed: File to read does not exist');
+  }
+
+
+  const fileContent = fs.readFileSync(fileToRead, 'utf-8');
+
+  console.log('File content:');
+  console.log(fileContent);
 };
 
 await read();
